b1323df5fa40726ea4fa9f12e1291c63d67f4374
Nicholas <thefullsnackdeveloper@gmail.com>
2019-08-14 12:17:28 -0400

    initial commit

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -1,21 +1,45 @@
 import {storiesOf} from "@storybook/react";
 import React from "react";
+import Avatar from '../Avatar';
 import Button from '../Button';
 import Card from "./";
 
+const avatar = <Avatar
+  raised
+  shape="circle"
+  image="https://images.unsplash.com/photo-1535441577682-5a7bc0702a7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80"/>;
+
 storiesOf("Card", module).add("default", () => (
-  <div className="story-spacer">
-    <Card
-      flat={false}
-      image="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80">
-      <h5>Card content here</h5>
-      <p>Add whatever you want in this area</p>
-      <Button
-        icon={{
-        symbol: 'chevron-right',
-        position: 'right'
-      }}>Read more</Button>
-    </Card>
+  <div style={{
+    display: 'flex'
+  }}>
+
+    <div className="story-spacer">
+      <Card
+        flat={false}
+        image="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80">
+        <h5>Card content here</h5>
+        <p>Add whatever you want in this area</p>
+        <Button
+          icon={{
+          symbol: 'chevron-right',
+          position: 'right'
+        }}>Read more</Button>
+      </Card>
+    </div>
+    <div className="story-spacer">
+      <Card
+        flat={false}
+        image="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80">
+        <h5>Card content here</h5>
+        <p>Add whatever you want in this area</p>
+        <Button
+          icon={{
+          symbol: 'chevron-right',
+          position: 'right'
+        }}>Read more</Button>
+      </Card>
+    </div>
   </div>
 )).add("with title", () => (
   <div className="story-spacer">
@@ -69,8 +93,7 @@ storiesOf("Card", module).add("default", () => (
     <div className="story-spacer">
       <Card
         center
-        title="Card Title"
-        flat={false}
+        avatar={avatar}
         image="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80">
         <h5>Card content here</h5>
         <p>Add whatever you want in this area</p>
@@ -83,4 +106,30 @@ storiesOf("Card", module).add("default", () => (
       </Card>
     </div>
   </div>
-));
+)).add("with avatar", () => (
+  <div className="story-spacer">
+    <Card
+      avatar={avatar}
+      image="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3750&q=80">
+      <h5>Card content here</h5>
+      <p>Add whatever you want in this area</p>
+      <Button
+        icon={{
+        symbol: 'chevron-right',
+        position: 'right'
+      }}>Read more</Button>
+    </Card>
+  </div>
+)).add("with no image", () => (
+  <div className="story-spacer">
+    <Card >
+      <h5>Card content here</h5>
+      <p>Add whatever you want in this area</p>
+      <Button
+        icon={{
+        symbol: 'chevron-right',
+        position: 'right'
+      }}>Read more</Button>
+    </Card>
+  </div>
+))
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,21 +4,39 @@ import "./Card.sass";
 
 class Card extends Component {
   render() {
-    const {title, image, flat, children, center} = this.props;
+    const {
+      title,
+      image,
+      flat,
+      children,
+      center,
+      avatar
+    } = this.props;
     const classes = classNames("Card", {
       flat
-    }, {center});
+    }, {
+      center,
+      withAvatar: avatar
+        ? true
+        : false
+    });
     return (
       <div className={classes}>
         {title && <div className="Card-title">
           <h2>{title}</h2>
         </div>}
-        <div
+        {image && <div
           className="Card-image"
           style={{
           backgroundImage: `url(${image})`
-        }}/>
-        <div className="Card-content">{children}</div>
+        }}/>}
+
+        <div className="Card-content">
+          {avatar && <div className="Card-avatar">
+            {avatar}
+          </div>}
+          {children}
+        </div>
       </div>
     );
   }
